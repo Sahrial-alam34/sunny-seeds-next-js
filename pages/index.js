57344c8c9c0f92d045734786e4ed956200a51608
Sahrial-alam34 <sunnykcp01@gmail.com>
2023-06-25 14:00:32 +0600

    section 1 added

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Header from '../components/header'
+import Format from '../layout/format';
+
+// compoenents
+import Section1 from '../components/section1';
 
 export default function Home() {
   return (
-   <div>
-      <Header></Header>
-   </div>
+   <Format>
+      <Section1></Section1>
+   </Format>
   )
 }
